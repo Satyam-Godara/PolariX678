c08032c8d3aac8c6d91f06c60b517578da6f4df4
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2026-03-11 01:49:46 +0530

    Replace MongoDB connection string with placeholder
    Updated MongoDB connection string to use a placeholder.

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const { createDummyExcelForAllUsers } = require('../utils/excelGenerator');
 const connectDB = async () => {
   try {
     // MongoDB Connection with improved error handling
-    await mongoose.connect('mongodb+srv://pulkitsachdeva:test123@polarx.wq0hut2.mongodb.net/polarix?retryWrites=true&w=majority&appName=polarX', {
+    await mongoose.connect('YOUR_MONGO_DB_ATLAS_URL', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
